4d439979b1aa2b1fcf464c600423553c13a4a949
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-08-29 21:13:44 +0200

    Remove conflict with DDDX balance mod

--- mod_info.lua
+++ mod_info.lua
@@ -1,16 +1,16 @@
 name = "Sky Tracker"
 uid = "11111122-1337-416a-9d76-aac3bb171b66"
 version = 1
-copyright = "DDDX, EntropyWins"
+copyright = "Burnie, DDDX, EntropyWins"
 description = "Adds the Sky Tracker super heavy anti air turret"
-author = "DDDX, EntropyWins"
+author = "Burnie, DDDX, EntropyWins"
 selectable = true
 ui_only = false
 enabled = true
 url = "https://github.com/JeroenDeDauw/SkyTracker"
 icon = "/mods/SkyTracker/icon.png"
 conflicts = {
-    "bcb7f667-e0d1-460f-aa54-f5e7d1589b42" -- Survival Mayhem&BO balance
+    -- "bcb7f667-e0d1-460f-aa54-f5e7d1589b42" -- Survival Mayhem&BO balance
 }
 
 
